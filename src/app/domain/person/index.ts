07b2806d988e061ba003e7ffa865919c59c139c7
kotyamur <99125819+kotyamur@users.noreply.github.com>
2023-09-08 15:10:13 +0300

    Update index.ts

--- src/app/domain/person/index.ts
+++ src/app/domain/person/index.ts
@@ -1,9 +1,1 @@
-import Person from "./person/Person";
 
-// Додаємо тип Controller для дотримання типів контролерів
-type Controller = typeof Person;
-
-// Додаємо контролер Person у масив controllers
-const controllers = <Controller[]>[Person];
-
-export { controllers };
